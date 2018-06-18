c2a0955f3464fea9c804b7b29613d8a21a0bed0c
Joel Parker <joelsparker@gmail.com>
2018-06-18 22:34:55 +0000

    Changes to gravelfinder.py routes
    Added /google to show google routes
    Add /test to show that flask works
    
    Route label_by_class() now uses a template
    Route first_map() shows class iv-vii roads on the same label

--- gravelfinder.py
+++ gravelfinder.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
+import config
 
 app = Flask(__name__)
 
+GOOGLE_API_KEY=config.GOOGLE_API_KEY
+
 @app.route('/')
 def index():
-    return app.send_static_file('embedded_map.html')
+    return redirect(url_for('label_by_class'))
+
+@app.route('/single_label')
+def first_map():
+    return render_template('map_class_iv_through_vii.html')
+
+@app.route('/label_by_class')
+def label_by_class():
+    return render_template('map_by_classifcation.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html',message='This is a test page.')
+
+@app.route('/google')
+def google_map():    
+    return render_template('google_map.html',google_api_key=GOOGLE_API_KEY)
 
 def main():
     pass

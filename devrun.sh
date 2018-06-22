ea2f8347e2d1a4e355dda27e986f1a959ca8f639
Joel Parker <joelsparker@gmail.com>
2018-06-22 18:27:52 +0000

    Have Devrun.sh show and check for GOOGLE_API_KEY environment variable

--- devrun.sh
+++ devrun.sh
@@ -1,4 +1,12 @@
 export FLASK_ENV=development
 export FLASK_APP=gravelfinder.py
+echo "Google API Key used is [$GOOGLE_API_KEY]"
+
+if [ "x$GOOGLE_API_KEY" = "x" ]; then
+    echo "SET the GOOGLE_API_KEY environment variable!"
+    echo "QUIT!"
+    exit 1
+fi
+
 ./geojson.py
 flask run --host=0.0.0.0 --port 8080
